2f1f4bc1b52402a63e1b94925ebc2edcdf62b536
kartik04111994 <shivkarthik.nov04@gmail.com>
2024-03-01 15:13:51 +0530

    pushing updated challenge-3

--- challenge-3/main.tf
+++ challenge-3/main.tf
@@ -3,7 +3,18 @@ variable "instance_config" {
   type = map
   default = {
     instance1 = { instance_type = "t2.micro", ami = "ami-03a6eaae9938c858c" }
-    instance2 = { instance_type = "t2.small", ami = "ami-053b0d53c279acc90" }
+    instance4 = { instance_type = "t2.small", ami = "ami-053b0d53c279acc90" }
+    instance3 = { instance_type = "t2.nano", ami = "ami-053b0d53c279acc90" }
   }
+}
+
+resource "aws_instance" "demoinstances" {
+  for_each = var.instance_config
+  ami = each.value.ami
+  instance_type = each.value.instance_type
+  tags = {
+    Name = "demoinstance-${each.key}"
+  }
+  
 }
 
